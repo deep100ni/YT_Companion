c8df9990fb77caf85385ddd4c51b85b36b9d5c40
Deep100ni <deep100ni13@gmail.com>
2025-08-16 11:36:04 +0530

    Refactor LocalRepo key
    The shared preference key string 'isLoggedIn' is replaced by a const `_KeyIsLoggedIn`.

--- lib/local_repo.dart
+++ lib/local_repo.dart
@@ -1,12 +1,14 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocalRepo{
+  static const _KeyIsLoggedIn = 'isLoggedIn';
+
   Future<void> onLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
-    prefs.setBool('isLoggrdIn', true);
+    prefs.setBool(_KeyIsLoggedIn, true);
   }
   Future<bool> isLoggedIn() async{
     final prefs = await SharedPreferences.getInstance();
-    return prefs.getBool('isLoggedIn') ?? false;
+    return prefs.getBool(_KeyIsLoggedIn) ?? false;
   }
 }
